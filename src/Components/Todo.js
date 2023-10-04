1d76795d0b36fa56fae0abfaf220a5d1976a8e87
Simran <simrangami07@gmail.com>
2023-10-04 15:56:44 +0545

    open file

---
+++ src/Components/Todo.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import {addTodo, deleteTodo, removeTodo} from '../actions/index';
+import {useDispatch} from 'react-redux';
+import "./todo.css";
+
+const todo = () => {
+  const [inputData, setInputData] = useState("");
+  const dispatch = useDispatch();
+
+
+  return (
+    <>
+      <div className="main-div">
+        <div className="child-div">
+          <figure>
+            <figcaption>Add your list here ✌🏼</figcaption>
+          </figure>
+
+          <div className="addItems">
+            <input
+              type="text"
+              placeholder="Add Items...."
+              value={inputData}
+              onChange={(event) => setInputData(event.target.value)}
+            />
+            <i className="fa fa-plus add-btn" onClick={() => dispatch(addTodo(inputData))}></i>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default todo;
---
+++ src/reducers/todoReducers.js
@@ -0,0 +1,26 @@
+const initialData = {
+  list: [],
+};
+
+// const [list, setList] = useState([])
+
+const todoReducers = (state = initialData, action) => {
+  switch (action.type) {
+    case "ADD_TODO":
+      const { id, data } = action.payload;
+
+      return {
+        ...state,
+        data: [
+          ...state.list,
+          {
+            id: id,
+            data: data,
+          },
+        ],
+      };
+    default:
+      return state;
+  }
+};
+export default todoReducers;
